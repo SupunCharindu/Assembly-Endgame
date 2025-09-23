b89d15cc83f09fd2e8c099820c37dfff9ab8257b
Supun Fernando <supuncharindu18@gmail.com>
2025-09-23 08:47:41 +0200

    Add game wining section to header part in main just hard code and stlye it

--- ASSEMBLY-ENDGAME/src/App.jsx
+++ ASSEMBLY-ENDGAME/src/App.jsx
@@ -14,6 +14,10 @@ export default function AssemblyEndgame() {
               <h1>Assembly: Endgame</h1>
               <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
             </header>
+            <section className="game-status">
+              <h2>You Win!</h2>
+              <p>Well done!🎉</p>
+            </section>
         </main>
     )
 }
